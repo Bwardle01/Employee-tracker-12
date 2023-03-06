211c725564e8431168319712e5f268eab66da4de
Bronson Wardle <bronson.wardle@gmail.com>
2023-03-06 16:40:51 -0500

    added functions in the server.js for showing employees, add a department, and add a role.

--- server.js
+++ server.js
@@ -83,9 +83,9 @@ function showDepartments(){
 };
 
 // will show all roles 
-function showRoles {
- const mysql = `SELECT role.id, role.title, department.name AS department, role.salary FROM role
-                INNER JOIN department ON role.department_id = department.id`;
+function showRoles() {
+ const mysql = `SELECT role.id, role.title, department.name AS department, role.salary    FROM role
+  INNER JOIN department ON role.department_id = department.id`;
 
   db.query(mySql,(err,results) => {
     if(err) throw err;
@@ -94,4 +94,96 @@ function showRoles {
       promptUser();
   })
  
+};
+
+//Will show all employess selected
+function showEmployees(){
+
+  const mySql = `SELECT employee.id, 
+              employee.first_name, 
+              employee.last_name, 
+              role.title, 
+              department.name AS department,
+              role.salary, 
+              CONCAT (manager.first_name, " ", manager.last_name) AS manager
+              FROM employee
+              LEFT JOIN role ON employee.role_id = role.id
+              LEFT JOIN department ON role.department_id = department.id
+              LEFT JOIN employee manager ON employee.manager_id = manager.id`;
+
+  db.query(mySql, (err, results) => {
+      if (err) throw err; 
+      console.log("Showing all employees");
+      console.table(results);
+      promptUser();
+  });
+};
+
+// Add a department
+function addDepartment(){
+  inquirer.prompt([
+      {
+          type: 'input', 
+          name: 'addDept',
+          message: "Please add a department",
+      }
+  ])
+      .then(answer => {
+      const mySql = `INSERT INTO department (name) VALUES (?)`;
+      db.query(mySql, answer.addDept, (err, result) => {
+          if (err) throw err;
+          console.log("----------------------------------------------".rainbow);
+          console.log(colors.green.bold('Added new Department:  ' + answer.addDept)); 
+          showDepartments();
+      });
+  });
+};
+
+// add a role
+function addRole(){
+  inquirer.prompt([
+      {
+          type: 'input', 
+          name: 'role',
+          message: "Enter the new role",
+      },
+      {
+          type: 'input', 
+          name: 'salary',
+          message: "Enter the salary of this role",
+      }
+  ])
+      .then(answer => {
+          const roleSalary = [answer.role, answer.salary];
+  
+        // getting the dept from dept table
+          const roleDb = `SELECT name, id FROM department`; 
+  
+          db.query(roleDb, (err, data) => {
+          if (err) throw err; 
+      
+          const dept = data.map(({ name, id }) => ({ name: name, value: id }));
+  
+          inquirer.prompt([
+          {
+              type: 'list', 
+              name: 'dept',
+              message: "Select the correct department for this new role.",
+              choices: dept
+          }
+          ])
+              .then(deptSelect => {
+              const dept = deptSelect.dept;
+              roleSalary.push(dept);
+  
+              const newRole = `INSERT INTO role (title, salary, department_id) VALUES (?, ?, ?)`;
+              db.query(newRole, roleSalary, (err, result) => {
+                  if (err) throw err;
+                  console.log("----------------------------------------------".rainbow);
+                  console.log(colors.green.bold('Added new Role:  ' + answer.role)); 
+              showRoles();
+          });
+      });
+  });
+  });
 };
